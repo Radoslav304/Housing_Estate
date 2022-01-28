eaacd292248107fd6ea845fac54ec85db0fe1b8f
Radoslav304 <radoslavcabadaj304@gmail.com>
2022-01-28 12:01:59 +0100

    Add project files.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HousingEstate
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+        public override string ToString()
+        {
+            return FirstName;
+        }
+
+
+
+
+
+    }
+}
+
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace HousingEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Radoslav";
+            Console.WriteLine(person.ToString());
+
+        }
+    }
+}
